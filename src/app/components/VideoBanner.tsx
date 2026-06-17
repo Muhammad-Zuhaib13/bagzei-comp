ed0488955b3a0036096b56c0cc5ef6876b12d3be
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2026-06-17 17:08:34 +0500

    updated

--- src/app/components/VideoBanner.tsx
+++ src/app/components/VideoBanner.tsx
@@ -2,10 +2,11 @@ import Link from "next/link"
 import ScreenContainer from "./ScreenContainer"
 const VideoBanner = () => {
     return (
-        <div className='w-full lg:h-[658px] md:h-[600px] h-[800px] relative'>
+        <div className='w-full lg:h-[658px] md:h-[600px] h-[800px] relative lg:pb-[70px] md:pb-[40px] pb-[60px]'>
+
             <video
                 src={'/assets/videos/bagei-home-banner-video.mp4'}
-                className="h-full w-full object-cover absolute inset-0"
+                className="h-full w-full object-cover absolute inset-0 "
                 autoPlay={true}
                 playsInline
                 muted
@@ -16,11 +17,17 @@ const VideoBanner = () => {
                 disablePictureInPicture
                 disableRemotePlayback
             />
-            <ScreenContainer element="section" className="relative z-10">
-                <div className="flex flex-col text-primary-1">
+            <div
+                className="absolute inset-0"
+                style={{
+                    background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 55.48%, rgba(0, 0, 0, 0.8) 100%)'
+                }}
+            />
+            <ScreenContainer element="section" className="relative z-10 flex flex-col justify-end">
+                <div className="flex flex-col lg:gap-[16px] md:gap-[24px] gap-[24px] text-primary-1">
                     <h1 className="font-banner-title-50 font-ChronicleDisplay">Meet the Mosaic Cabas</h1>
-                    <p className="font-banner-text-18 font-Akkurat">Our new tote designed for everything the day holds.</p>
-                    <Link className="font-banner-link-18 font-Akkurat uppercase font-medium " href='/#'>EXPLORE THE COLLECTION</Link>
+                    <p className="font-banner-text-18 font-Akkurat md:w-fit w-[241px] ">Our new tote designed for everything the day holds.</p>
+                    <Link className="font-banner-link-18 font-Akkurat uppercase font-medium underline underline-offset-3 " href='/#'>EXPLORE THE COLLECTION</Link>
                 </div>
             </ScreenContainer>
         </div>
