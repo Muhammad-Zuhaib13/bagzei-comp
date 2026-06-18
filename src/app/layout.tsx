4a58b71c8d9e0647a976b31e11784abeb0c969f4
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2026-06-18 18:44:20 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${Akkurat.variable} ${AkkuratBold.variable} ${ChronicleDisplay.variable} antialiased`}
       >
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
         <Footer/>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="w-full">
       <VideoBanner />
-      <TwoCardsSection />
       <NewInSection />
+      <TwoCardsSection />
     </div>
   );
 }
